a7a3341235c40c492da7e35953a7a1057f034a32
Maria Necheporenko <mari.necheporenko@gmail.com>
2022-12-24 16:05:26 +0200

    change time in messages

--- src/components/message/index.ts
+++ src/components/message/index.ts
@@ -13,7 +13,28 @@ export default class Message extends Block {
     super(props);
   }
 
+  setZero(num: number) {
+    if (num < 10) {
+      return `0${num}`
+    }
+     
+    return num;
+  } 
+
+  time(time: string) {
+    const date = new Date(time);
+
+    const hours = this.setZero(date.getHours());
+    const minutes = this.setZero(date.getMinutes());
+
+    return `${hours}:${minutes}`;
+  }
+
   render() {
-    return this.compile(template, {...this.props, styles});
+    return this.compile(template, {
+      ...this.props,
+      time: this.time(this.props.time),
+      styles
+    });
   }
 }
